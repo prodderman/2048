0147fc7267624a634cd69a41c0f9a0c3c83ecc37
Stepan <ranibowprodder@gmail.com>
2019-06-17 00:10:14 +0700

    add Keyboard manager

---
+++ src/view/KeyboardManager.svelte
@@ -0,0 +1,26 @@
+<svelte:window on:keydown={handleKeydown}/>
+<script>
+  export let move;
+
+  const keyCodesMap = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+  }
+
+  function handleKeydown(event) {
+    const modifiers = 
+      event.altKey || 
+      event.ctrlKey || 
+      event.metaKey ||
+      event.shiftKey;
+
+    const key = keyCodesMap[event.keyCode];
+
+    if (!modifiers && key) {
+      move(key);
+    }
+  }
+</script>
+
